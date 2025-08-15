2fbde754173a0f38290e6af47422223cf76acee5
brahimtalebathmane <brahimtalebathmane@gmail.com>
2025-08-15 10:28:03 -1200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Firebase messaging service worker
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
 // Initialize Firebase in service worker
 firebase.initializeApp({
