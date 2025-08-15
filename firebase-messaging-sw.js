ea38a3305f5eee0db36ec83133cf5ad2256f2999
brahimtalebathmane <brahimtalebathmane@gmail.com>
2025-08-15 09:55:22 -1200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 
 // Initialize Firebase in service worker
 firebase.initializeApp({
-  apiKey: "VITE_FIREBASE_API_KEY_PLACEHOLDER",
-  authDomain: "VITE_FIREBASE_AUTH_DOMAIN_PLACEHOLDER", 
-  projectId: "VITE_FIREBASE_PROJECT_ID_PLACEHOLDER",
-  storageBucket: "VITE_FIREBASE_STORAGE_BUCKET_PLACEHOLDER",
-  messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER",
-  appId: "VITE_FIREBASE_APP_ID_PLACEHOLDER"
+  apiKey: "AIzaSyCAncc8um-yQBA1VzATvHAbCPOCPo5F_1E",
+  authDomain: "lawdli.firebaseapp.com", 
+  projectId: "lawdli",
+  storageBucket: "lawdli.firebasestorage.app",
+  messagingSenderId: "1095350923790",
+  appId: "1:1095350923790:web:93631ae307a6ecdce5425f"
 });
 
 const messaging = firebase.messaging();
